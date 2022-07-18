9e36b3fa3591d648ab9c42dd61491a8c002f50d7
Pedro Lérida Nieto <pedroleridanieto@hotmail.com>
2022-07-18 15:09:35 +0200

    Iteration#5

--- app.js
+++ app.js
@@ -331,11 +331,25 @@ console.log(contadorVolume);
 
   findArrayIndex (animales, "Salamandra")
 
+/*------------------------------------------------------------------------
+ --------------------**iteración #5 : Función rollDice-------------------
+ -----------------------------------------------------------------------*/
+ /*Crea una función llamada **rollDice()** que reciba como parametro el numero de caras que queramos 
+ que tenga el dado que deberá silumar el codigo dentro de la función. Como hemos dicho, que la función 
+ use el parametro para simular una tirada de dado y retornar el resultado. Si no se te ocurre como hacer 
+ un numero aleatorio no te preocupes! busca información sobre la función de javascript **Math.random();**/
 
 
 
-  
+const rollDice =(caras)=>{
 
+  aleatorio=Math.floor(Math.random()*caras)+1;
+
+  return aleatorio
+
+}
+
+console.log (rollDice(7));
 
 
 
